c89b67a137ad0de849afae8a6318bd5de6440bd9
TasteTheThunder <1MS23IS005@msrit.edu>
2026-05-12 11:23:51 +0530

    Initial bad code commit

---
+++ BadCode.java
@@ -0,0 +1,47 @@
+public class BadCode {
+
+    public static void main(String[] args) {
+
+        String password = "admin123";
+
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        if(z > 0){
+            if(z > 5){
+                if(z > 10){
+                    System.out.println("Very big");
+                }
+            }
+        }
+
+        try{
+            int a = 10/0;
+        }catch(Exception e){
+
+        }
+
+        unusedMethod();
+    }
+
+    public static void unusedMethod(){
+        System.out.println("unused");
+    }
+
+    public static void duplicate1(){
+        int sum = 0;
+        for(int i=0;i<100;i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    public static void duplicate2(){
+        int sum = 0;
+        for(int i=0;i<100;i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
